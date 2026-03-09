c499bdc7081f70f5edbdb8ed09c4aca167e154ba
HaruDanNoe <165749431+HaruDanNoe@users.noreply.github.com>
2026-03-09 15:24:30 +0800

    register2

--- team-cluster2-main/frontend/src/pages/Login.jsx
+++ team-cluster2-main/frontend/src/pages/Login.jsx
@@ -84,7 +84,7 @@ export default function Login() {
         </button>
 
         <p className="auth-footer">
-          No account? <a href="/Register">Register</a>
+          No account? <a href="Register.jsx">Register</a>
         </p>
       </form>
     </AuthLayout>
