e3a90eba4da283587fd36c7f8cb7c70e2536bedd
HaruDanNoe <165749431+HaruDanNoe@users.noreply.github.com>
2026-03-09 15:31:21 +0800

    registr4

--- team-cluster2-main/frontend/src/main.jsx
+++ team-cluster2-main/frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/app.css";
+import Register from "./pages/Register";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- team-cluster2-main/frontend/src/pages/Login.jsx
+++ team-cluster2-main/frontend/src/pages/Login.jsx
@@ -84,7 +84,7 @@ export default function Login() {
         </button>
 
         <p className="auth-footer">
-          No account? <a href="./register">Register</a>
+          No account? <a href="/register">Register</a>
         </p>
       </form>
     </AuthLayout>
