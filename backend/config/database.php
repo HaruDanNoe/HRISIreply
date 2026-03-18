2304dc7a5e199cf0d697553bc0224322a7368c23
Dante Filasol Jr. <142869485+dtfj03@users.noreply.github.com>
2026-03-18 16:36:23 +0800

    Update db config
    Co-Authored-By: andræ <167016980+aelreje@users.noreply.github.com>

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "system_hris_db");
+$conn = new mysqli("localhost:3306", "dtfj_system_hris_db", "ireplyusls", "dtfj_system_hris_db");
 
 if ($conn->connect_error) {
     http_response_code(500);
